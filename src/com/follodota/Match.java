eba49a4cecabed2ab4d355908e729e2adea625c9
Keang <skeang@gmail.com>
2014-02-21 01:55:52 +0800

    added networking library

---
+++ src/com/follodota/Match.java
@@ -0,0 +1,6 @@
+package com.follodota;
+
+public class Match {
+
+	
+}
--- src/com/follodota/dummy/DummyContent.java
+++ src/com/follodota/dummy/DummyContent.java
@@ -25,9 +25,9 @@ public class DummyContent {
 
     static {
         // Add 3 sample items.
-        addItem(new DummyItem("1", "Item 1"));
-        addItem(new DummyItem("2", "Item 2"));
-        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Navi vs Tongfu"));
+        addItem(new DummyItem("2", "Alliance vs Fnatic"));
+        addItem(new DummyItem("3", "LGD vs Virtus Pro"));
     }
 
     private static void addItem(DummyItem item) {
